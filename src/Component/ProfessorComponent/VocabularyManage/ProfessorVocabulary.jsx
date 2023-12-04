782dd7fef8d383ee74a19a1ba479092b832d55e7
Yoo2309 <dutran2392002@gmail.com>
2023-12-04 11:05:06 +0700

    vocabulary-fix

--- src/Component/ProfessorComponent/VocabularyManage/ProfessorVocabulary.jsx
+++ src/Component/ProfessorComponent/VocabularyManage/ProfessorVocabulary.jsx
@@ -94,6 +94,10 @@ function ProfessorVocabulary() {
     fetchVocabularyTopic();
     window.scrollTo(0, 0);
   }, []);
+  useEffect(() => {
+    fetchVocabulary();
+    window.scrollTo(0, 0);
+  }, [modal]);
   const handleUpdateVocabularyTopic = async (register) => {
     console.log(register.name, id, user.userId);
     setIsLoading(true);
@@ -143,48 +147,49 @@ function ProfessorVocabulary() {
   };
   const handleDeleteVocabulary = async (id) => {
     console.log(id);
-    // setIsLoading(true);
-    // try {
-    //   const response = await fetch(
-    //     `https://localhost:7112/api/VocTopic/UpdateVocTopic/${id}`,
-    //     {
-    //       method: "DELETE",
-    //       headers: {
-    //         "Content-Type": "application/json",
-    //       },
-    //       body: JSON.stringify({}),
-    //     }
-    //   );
-    //   setIsLoading(false);
-    //   if (!response.ok) {
-    //     const errorData = await response.json();
-    //     console.log(response);
-    //     toast.error(`${errorData.message}`, {
-    //       position: toast.POSITION.BOTTOM_RIGHT,
-    //       autoClose: 5000,
-    //       closeOnClick: true,
-    //       pauseOnHover: true,
-    //       draggable: true,
-    //     });
-    //   } else {
-    //     toast.success("Delete Topic Successfully", {
-    //       position: toast.POSITION.BOTTOM_RIGHT,
-    //       autoClose: 10000,
-    //       closeOnClick: true,
-    //       pauseOnHover: true,
-    //       draggable: true,
-    //     });
-    //   }
-    // } catch (error) {
-    //   toast.error(`${error}`, {
-    //     position: toast.POSITION.BOTTOM_RIGHT,
-    //     autoClose: 3000,
-    //     hideProgressBar: false,
-    //     closeOnClick: true,
-    //     pauseOnHover: true,
-    //     draggable: true,
-    //   });
-    // }
+    setIsLoading(true);
+    try {
+      const response = await fetch(
+        `https://localhost:7112/api/Vocabulary/DeleteVocabulary/${id}`,
+        {
+          method: "DELETE",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({}),
+        }
+      );
+      setIsLoading(false);
+      if (!response.ok) {
+        const errorData = await response.json();
+        console.log(response);
+        toast.error(`${errorData.message}`, {
+          position: toast.POSITION.BOTTOM_RIGHT,
+          autoClose: 5000,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+        });
+      } else {
+        toast.success("Delete Topic Successfully", {
+          position: toast.POSITION.BOTTOM_RIGHT,
+          autoClose: 10000,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+        });
+        fetchVocabulary();
+      }
+    } catch (error) {
+      toast.error(`${error}`, {
+        position: toast.POSITION.BOTTOM_RIGHT,
+        autoClose: 3000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+      });
+    }
   };
 
   if (isLoading) {
@@ -192,61 +197,67 @@ function ProfessorVocabulary() {
   }
   return (
     <>
-    <AddVocabulary toggleModal={toggleModal} modal_on={modal} idVoc={id}/>
-    <div className="professor-vocabulary">
-      <div className="professor-managment-title">
-        <h3>QUẢN LÝ CHỦ ĐỀ TỪ VỰNG</h3>
-      </div>
+      <AddVocabulary toggleModal={toggleModal} modal_on={modal} idVoc={id} />
+      <div className="professor-vocabulary">
+        <div className="professor-managment-title">
+          <h3>QUẢN LÝ CHỦ ĐỀ TỪ VỰNG</h3>
+        </div>
 
-      <form onSubmit={handleSubmit(handleUpdateVocabularyTopic)}>
-        <div style={{ width: "100%", textAlign: "center" }}>
-          <div className="input-field">
-            <input
-              type="text"
-              defaultValue={curent_topic.name}
-              onFocus={() => setShowButton(true)}
-              {...vocabulary_topic("name", { required: true })}
-            />
-          </div>
-          <error>
-            {errors.name?.type === "required" && "Không được để trống tên"}
-          </error>
-        </div>
-        {showButton && (
-          <input
-            type="submit"
-            className="vocabulary-submit"
-            value="Cập nhật"
-          ></input>
-        )}
-      </form>
-      <div className="professor-add-button-wrapper">
-        <div className="professor-add-button" onClick={toggleModal}>
-          <img
-            width="34"
-            height="34"
-            src="https://img.icons8.com/doodle/48/add.png"
-            alt="add"
-          />
-          <h3>THÊM TỪ MỚI</h3>
-        </div>
-      </div>
-      <div className="wordList-wrapper">
-        {words && words.map((word, index) => {
-          return (
-            <div key={index} className="wordList-item">
-              <div className="eng-word">{word.engWord}</div>
-              <div className="word-type">{word.wordType}</div>
-              <div className="word-meaning">{word.meaning}</div>
-              <div className="btn-wrapper">
-                <button className="delete-btn" onClick={() => handleDeleteVocabulary(word.idVoc)}>Xóa</button>
-                <button className="update-btn">Sửa</button>
-              </div>
+        <form onSubmit={handleSubmit(handleUpdateVocabularyTopic)}>
+          <div style={{ width: "100%", textAlign: "center" }}>
+            <div className="input-field">
+              <input
+                type="text"
+                defaultValue={curent_topic.name}
+                onFocus={() => setShowButton(true)}
+                {...vocabulary_topic("name", { required: true })}
+              />
             </div>
-          );
-        })}
+            <error>
+              {errors.name?.type === "required" && "Không được để trống tên"}
+            </error>
+          </div>
+          {showButton && (
+            <input
+              type="submit"
+              className="vocabulary-submit"
+              value="Cập nhật"
+            ></input>
+          )}
+        </form>
+        <div className="professor-add-button-wrapper">
+          <div className="professor-add-button" onClick={toggleModal}>
+            <img
+              width="34"
+              height="34"
+              src="https://img.icons8.com/doodle/48/add.png"
+              alt="add"
+            />
+            <h3>THÊM TỪ MỚI</h3>
+          </div>
+        </div>
+        <div className="wordList-wrapper">
+          {words &&
+            words.map((word, index) => {
+              return (
+                <div key={index} className="wordList-item">
+                  <div className="eng-word">{word.engWord}</div>
+                  <div className="word-type">{word.wordType}</div>
+                  <div className="word-meaning">{word.meaning}</div>
+                  <div className="btn-wrapper">
+                    <button
+                      className="delete-btn"
+                      onClick={() => handleDeleteVocabulary(word.idVoc)}
+                    >
+                      Xóa
+                    </button>
+                    <button className="update-btn">Sửa</button>
+                  </div>
+                </div>
+              );
+            })}
+        </div>
       </div>
-    </div>
     </>
   );
 }
